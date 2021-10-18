c44ae4fa4017efbdd9b1decf62dd28ec63c4009a
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-10-18 08:54:04 +0200

    commiting changes

---
+++ index.js
@@ -0,0 +1,16 @@
+function openMenu() {
+    document.getElementById("menu_nav").style.display = "block";
+    document.getElementById("title").style.display = "none";
+    document.getElementById("text").style.display = "none";
+    document.getElementById("start").style.display = "none";
+    document.getElementById("learn").style.display = "none";
+
+}
+function closeMenu() {
+    document.getElementById("menu_nav").style.display = "none";
+    document.getElementById("title").style.display = "block";
+    document.getElementById("text").style.display = "block";
+    document.getElementById("start").style.display = "block";
+    document.getElementById("learn").style.display = "block";
+
+}
