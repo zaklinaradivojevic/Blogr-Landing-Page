9fd484768178376ef731caae938751d4a3e93901
zaklinaradivojevic <zaklinaradivojevic77777777@gmail.com>
2021-10-23 11:54:34 +0200

    changing footer

--- index.js
+++ index.js
@@ -4,9 +4,6 @@ function openMenu() {
     document.getElementById("text").style.display = "none";
     document.getElementById("start").style.display = "none";
     document.getElementById("learn").style.display = "none";
-    document.getElementById("menu").style.display = "none";
-    document.getElementById("close").style.display = "block";
-
 }
 function closeMenu() {
     document.getElementById("menu_nav").style.display = "none";
@@ -14,8 +11,4 @@ function closeMenu() {
     document.getElementById("text").style.display = "block";
     document.getElementById("start").style.display = "block";
     document.getElementById("learn").style.display = "block";
-    document.getElementById("menu").style.display = "block";
-    document.getElementById("close").style.display = "none";
-
-
 }
